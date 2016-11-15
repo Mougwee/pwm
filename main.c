57c428baf4fd62ad6bdc1615d599a9217086e58a
Mougwee <dav.flei.91@gmail.com>
2016-11-15 11:28:41 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -18,43 +18,36 @@
  */
 #include "platform.h" /* include peripheral declarations */
 
-#define TIMER1_MODULO 31249
-#define DUTY_CYCLE_DIVISOR 20
-
-interrupt void timerOverflowInt(void){
-	//Timer-Overflow zurücksetzen, das System würde sonst ewig wiederholend in die ISR springen
-	TPM1SC_TOF = 0;
-}
-
-interrupt void channel2OverflowInt(void){
-	TPM1C2SC_CH2F = 0;
-}
 
 void initPorts(void){
 	//Lichter initialisieren
 	PTDD_PTDD2 = 1;		//Rücklichter einschalten (Ein = 1)
 	PTDDD_PTDDD2 = 1;	//Rücklicht-Ports als Output
+	
+	PTED_PTED7 = 0;
+	PTEDD_PTEDD7 = 1;
 }
 
 void initTimer(void){
 	//Timer einstellen
 	// (MOD + 1) * PS / f
-	TPM1SC_PS = 5;				//Prescaler 32
-	TPM1MOD = TIMER1_MODULO;	//Modulo
+	TPM1SC_PS = 7;				//Prescaler 128
+	TPM1MOD = 110;				//Modulo
 	//Timer Reset, zur Sicherheit
 	TPM1CNT = 0;				//clear counter register
 	TPM1SC_TOF = 0;				//clear overflow flag
-	//Timer-Interrupt einschalten, aktiviert ISR (gemäss Linker) wenn TOF = 1
-	TPM1SC_TOIE = 1;
+	//Timer-Interrupt
+	//TPM1SC_TOIE = 0;
 	
 	//Channel einstellen, Buzzer an TPM1CH5
-	TPM1C2V = (TIMER1_MODULO / DUTY_CYCLE_DIVISOR);
-	TPM1C2SC_MS2x = 2;	//Mode: edge-aligned PWM
-	TPM1C2SC_ELS2x = 2;	//Mode: clear output on compare
+	
+	TPM1C2SC_MS2B = 1;	//Mode: edge-aligned PWM
+	TPM1C2SC_ELS2B = 1; //Mode: clear output on compare
+	TPM1C2SC_ELS2A = 1;
 	TPM1C2SC_CH2F = 0;
 	//Channel-Interrupt
-	TPM1C2SC_CH2IE = 1;
-	
+	//TPM1C2SC_CH2IE = 0;
+	TPM1C2V = 8;
 	
 	/*
 	 * Der Timer fängt an zu zählen, sobald die Clock gewählt wird,
@@ -74,7 +67,7 @@ void main(void)
     initTimer();
 	
 	// Interrupts aktivieren, I-Bit in CCR-Register (0 = eingeschaltet)
-    EnableInterrupts;
+    //EnableInterrupts;
     
     
     for(;;) 
